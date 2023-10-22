71783caf801d8ae1849c513e7047e59f52756404
Jin Lee <jin@hjlee.co>
2023-10-21 20:11:04 -0700

    refine js30 01

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,61 +1,21 @@
-const [divA, divS, divD, divF, divG, divH, divJ, divK, divL] = document.querySelectorAll('div.key');
-const [audioA, audioS, audioD, audioF, audioG, audioH, audioJ, audioK, audioL] = document.querySelectorAll('audio');
+const keyDivs = document.querySelectorAll('div.key');
+
+const removePlayingClass = (e) => {
+    e.propertyName === 'transform' && e.target.classList.remove('playing');
+}
+
+keyDivs.forEach(div => {
+    div.addEventListener('transitionend', removePlayingClass);
+})
 
 const keydownEvent = (event) => {
-    const key = event.key;
-    if (key === 'a' || key === 'A') {
-        divA.classList.add('playing');
-        audioA.play().then(() => {
-            divA.classList.remove('playing');
-        })
-    }
-    if (key === 's' || key === 'S') {
-        divS.classList.add('playing');
-        audioS.play().then(() => {
-            divS.classList.remove('playing');
-        })
-    }
-    if (key === 'd' || key === 'D') {
-        divD.classList.add('playing');
-        audioD.play().then(() => {
-            divD.classList.remove('playing');
-        })
-    }
-    if (key === 'f' || key === 'F') {
-        divF.classList.add('playing');
-        audioF.play().then(() => {
-            divF.classList.remove('playing');
-        })
-    }
-    if (key === 'g' || key === 'G') {
-        divG.classList.add('playing');
-        audioG.play().then(() => {
-            divG.classList.remove('playing');
-        })
-    }
-    if (key === 'h' || key === 'H') {
-        divH.classList.add('playing');
-        audioH.play().then(() => {
-            divH.classList.remove('playing');
-        })
-    }
-    if (key === 'j' || key === 'J') {
-        divJ.classList.add('playing');
-        audioJ.play().then(() => {
-            divJ.classList.remove('playing');
-        })
-    }
-    if (key === 'k' || key === 'K') {
-        divK.classList.add('playing');
-        audioK.play().then(() => {
-            divK.classList.remove('playing');
-        })
-    }
-    if (key === 'l' || key === 'L') {
-        divL.classList.add('playing');
-        audioL.play().then(() => {
-            divL.classList.remove('playing');
-        })
+    const code = event.code;
+    const divEl = document.querySelector(`div[data-key=${code}]`);
+    const audioEl = document.querySelector(`audio[data-key=${code}]`);
+    if (divEl) {
+        divEl.classList.add('playing');
+        audioEl.currentTime = 0;
+        audioEl.play();
     }
 }
 
